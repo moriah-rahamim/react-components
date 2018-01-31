3f4682e1e4b2bfa14cd4115eb97bcc6644f3e4ca
Moriah Rahamim <moriahfaye@gmail.com>
2018-01-31 11:12:12 -0800

    implement/render GroceryList component plus two grocery item components

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+var AlmondButter = () => (
+  <li>Almond Butter</li>
+);
+
+var Apples = () => (
+  <li>3 Granny Smith Apples, 3 Fuji Apples</li>
+);
+
+var GroceryList = () => (
+  <ul>
+    <AlmondButter />
+    <Apples />
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
